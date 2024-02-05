bf7f72b84e9e0e3d9e24873b47290782241ce21d
Joao Victor Costa <victor_ngl2@hotmail.com>
2024-02-05 18:40:06 -0300

    [UPDATE] Save on strapi BD

--- src/modules/catraca/catraca-move.service.ts
+++ src/modules/catraca/catraca-move.service.ts
@@ -17,7 +17,10 @@ export class CatracaServiceMove {
     }
 
     try {
+
+
       const gateMove = await this.createGateMove(data)
+
       if (gateMove) {
         return true;
       }
@@ -27,14 +30,35 @@ export class CatracaServiceMove {
     }
     catch (e) {
       throw new HttpException('Erro ao salvar Movimento da Catraca no Banco de Dados', HttpStatus.BAD_REQUEST)
-    
+
     }
   }
 
   async createGateMove(data: Prisma.GateMoveCreateInput): Promise<GateMove> {
-    return this.prisma.gateMove.create({
-      data,
-    });
-  }
 
+    const data_json = {
+      "data": {
+        student_id: data.studentId.toString(),
+        student_name: data.studentName,
+        schedule: data.schedule,
+        type: data.type
+      }
+    }
+
+    const response = await fetch('https://api.cambauba.org.br/api/gate-moves/', {
+      cache: 'no-store',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.STRAPI_JWT}`,
+      },
+      body: JSON.stringify(data_json),
+    });
+
+    if (!response.ok) {
+      throw new Error(`Erro ao realizar a requisição: ${response.statusText}`);
+    }
+
+    return await response.json();
+  }
 }
